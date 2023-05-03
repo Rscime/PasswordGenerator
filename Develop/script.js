a47db705ef724ce31c2a98e7fad2c662dcd67018
Rscime <Rachelscime@gmail.com>
2023-05-02 22:07:31 -0400

    password generated. need to write to webpage still

--- Develop/script.js
+++ Develop/script.js
@@ -5,11 +5,12 @@ let numbers = ['0','1','2','3','4','5','6','7','8','9'];
 let capital = ['A','B','C','D','E','F','G','H','I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 let lowercase = ['a','b','c','d','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 let special = ['!','#','$','%','&','*','@','+','-','_','<','>'];
+let passwordOptions = [];
 //Function for password criteria prompt
 function generatePassword() {
   //Password Length
   let userLength = prompt("How long would you like your password? (Please insert a whole number between 8 and 128)");
-  //obtain and verify length, Uppper, Lower, Numbers, Special Characters
+  //obtain and verify length
   if (userLength < 8 || userLength  > 128) {
     userLength  = prompt("Your input was invalid, please select a number between 8 and 128 for your password");
   }
@@ -17,40 +18,35 @@ function generatePassword() {
       window.alert("You selected a password length of: " + userLength);
     }
   
-  let userCapital = prompt("Would you like the password to have a capital letter? (enter Y for yes or enter N for no");
-  if (userCapital ==! Y || userCapital ==! N){
-      userCapital = prompt ("Your input was invalid. If you would like your password to have a capital letter please insert Y, if you would not like your password to have a capital letter please insert N.");
-  }
-    else{
-      window.alert("You selected " + userCapital + "for capital letters");
-    }
+  //Add to array containing possible password characters with Y/N confirm statements
+ let userCapital = confirm("Can your password have Capital letters?");
+ if(userCapital){
+  passwordOptions = passwordOptions.concat(capital);
+ }
 
-  let userLower = prompt("Would you like your password to have a lowercase letter? (enter Y for yes or enter N for no");
-  if (userLower ==! Y || userLower ==! N){
-       userLower = prompt ("Your input was invalid. If you would like your password to have a lowercase letter please insert Y, if you would not like your password to have a lowercase letter please insert N.");
-  }
-    else{
-      window.alert("You selected " + userLower + "for lowercase letters");
-    }
-  
-  let userNumber = prompt("Would you like your password to have a number? (enter Y for yes or enter N for no");
-  if (userNumber ==! Y || userNumber ==! N){
-      userNumber = prompt ("Your input was invalid. If you would like your password to have a number please insert Y, if you would not like your password to have a number please insert N.");
-  }
-    else{
-      window.alert("You selected " + userNumber + "for numbers");
-    }  
-      
-  let userSpecial = prompt("Would you like your password to have a special character? (enter Y for yes or enter N for no");
-  if (userSpecial ==! Y || userSpecial ==! N){
-      userSpecial = prompt ("Your input was invalid. If you would like your password to have a special character please insert Y, if you would not like your password to have a special character please insert N.");
-  }
-    else{
-      window.alert("You selected " + userSpecial + "for special characters");  
-    }
+ let userLower = confirm("Can your password have lowercase letters?");
+ if(userLower){
+  passwordOptions = passwordOptions.concat(lowercase);
+ }
 
-  }
-//Start generating a password based on inputs (length, capital, lowercase, number, special)
+ let userNumber = confirm("Can your password have numbers?");
+ if(userNumber){
+  passwordOptions = passwordOptions.concat(numbers);
+ }
+
+ let userSpecial = confirm("Can your password have special characters?");
+ if(userSpecial){
+  passwordOptions = passwordOptions.concat(special);
+  //Check array
+  // console.log(passwordOptions);
+ }
+
+//Create random password from options above
+for(var i=0; i<=userLength; i++){
+let passwordCharacters = passwordOptions[Math.floor(Math.random()*passwordOptions.length)];
+console.log(passwordCharacters);
+}
+}
 
 
 // Write password to the #password input
